7efa7d823829852922f18de9705be44138588819
alucardlockon <alucardlockon3@gmail.com>
2017-01-04 14:19:50 +0800

    commit 2017/1/4

---
+++ RestfulNews/RestfulNews.Tests/Normal/NormalTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+
+namespace RestfulNews.Tests.normal
+{
+    [TestClass]
+    public class NormalTest
+    {
+        [TestMethod]
+        public void TestDateTime()
+        {
+            string rqStr = "1212";
+            string yxq_val = "yym".Replace("m", "M");
+            DateTime datetime;
+            //检测日期格式是否正确
+            if(DateTime.TryParseExact(rqStr,yxq_val, CultureInfo.InvariantCulture, DateTimeStyles.None,out datetime))
+            //if (DateTime.TryParse(rqStr, out datetime))
+            //if (DateTime.TryParse(rqStr, info, DateTimeStyles.None, out datetime))
+            {
+            }else
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}

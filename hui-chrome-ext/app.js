8c530c1f3df8dc96ffe57e775cedcee6c0ebe1b3
alucardlockon <alucardlockon3@gmail.com>
2017-01-28 13:11:12 +0800

    2017-01-28

--- hui-chrome-ext/app.js
+++ hui-chrome-ext/app.js
@@ -1,11 +1,21 @@
-/*
-var contexts = ["page","selection","link","editable","image","video",
-                "audio"];
+var contexts = ["page", "selection", "link", "editable", "image", "video",
+    "audio"
+]
 for (var i = 0; i < contexts.length; i++) {
-  var context = contexts[i];
-  var title = "Test '" + context + "' menu item";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                       "onclick": genericOnClick});
-  console.log("'" + context + "' item:" + id);
+    var context = contexts[i]
+    var title = "复制页面名称"
+    var id = chrome.contextMenus.create({
+        "title": title,
+        "contexts": [context],
+        "onclick": copylink
+    })
 }
-*/
+
+function copylink() {
+    let url = ''
+    alert(chrome.tabs.length)
+    chrome.tabs.getCurrent(function(tab) {
+        url = tab.url
+    })
+    alert(url)
+}
